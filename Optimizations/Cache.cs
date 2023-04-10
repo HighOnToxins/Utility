acfc2792ef6861f84f8ac276ace6f907602addcb
Toxic <markusbolsen@gmail.com>
2023-04-10 15:29:54 +0200

    Added optimizations projects, incl. Cache class.

---
+++ Optimizations/Cache.cs
@@ -0,0 +1,65 @@
+﻿
+namespace Optimizations;
+
+public abstract class Cache<TResult>
+{
+
+    private TResult? _value;
+
+    protected abstract TResult Assign();
+
+    public TResult Get()
+    {
+        _value ??= Assign();
+        return _value;
+    }
+
+    public bool IsCached()
+    {
+        return _value != null;
+    }
+}
+
+public abstract class Cache<T, TResult> where T : notnull
+{
+
+    private readonly Dictionary<T, TResult> _values;
+
+    public Cache()
+    {
+        _values = new Dictionary<T, TResult>();
+    }
+
+    protected abstract TResult Assign(T arg);
+
+    public TResult Get(T arg)
+    {
+        if(!_values.TryGetValue(arg, out TResult? value) || value == null)
+        {
+            value = Assign(arg);
+            _values.Add(arg, value);
+        }
+
+        return value;
+    }
+
+    public bool IsCached(T arg)
+    {
+        return _values.ContainsKey(arg);
+    }
+}
+
+public sealed class FuncCache<T, TResult>: Cache<T, TResult> where T : notnull
+{
+    private readonly Func<T, TResult> _assign;
+
+    public FuncCache(Func<T, TResult> assign) : base()
+    {
+        _assign = assign;
+    }
+
+    protected override TResult Assign(T arg)
+    {
+        return _assign.Invoke(arg);
+    }
+}
