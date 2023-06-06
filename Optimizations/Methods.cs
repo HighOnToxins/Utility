f307921a3429baceca22cee402019663b8ea6289
Toxic <markusbolsen@gmail.com>
2023-06-06 22:05:48 +0200

    Added optimized methods.

---
+++ Optimizations/Methods.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace Optimizations;
+
+public static class Methods
+{
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static unsafe int ToInt(this bool b) => *(int*)&b;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Max(int a, int b) => Lerp(a > b, a, b);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Min(int a, int b) => Lerp(a < b, a, b);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Lerp(float inter, int start, int end) => (int)((end - start) * inter) + start;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int Lerp(bool inter, int start, int end) => (end - start) * inter.ToInt() + start;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double Lerp(double inter, double start, double end) => (end - start) * inter + start;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static double IntLerp(bool inter, double start, double end) => (end - start) * inter.ToInt() + start;
+
+}
