e6875284bd141f7afd69e7810977c205f230f62b
Toxic <markusbolsen@gmail.com>
2023-09-13 07:40:04 +0200

    Changed universe to be an interface, and of fixed size. Change my bit array to bit array 64 since it uses ulongs.

---
+++ DataStructures/bitStructures/BitArray64.cs
@@ -0,0 +1,83 @@
+﻿using Optimizations;
+using System;
+
+namespace DataStructures.bitStructures;
+
+public class BitArray64
+{
+    private readonly ulong[] values;
+
+    public BitArray64(int length)
+    {
+        Length = length;
+        values = new ulong[length / sizeof(ulong)];
+    }
+
+    private BitArray64(int length, ulong[] values)
+    {
+        Length = length;
+        this.values = values;
+    }
+
+    public int Length;
+
+    public bool this[int index]
+    {
+        get => Get(index);
+        set => Set(index, value);
+    }
+
+    public bool Get(int index)
+    {
+        int listIndex = index / sizeof(ulong);
+        int bitIndex = index % sizeof(ulong);
+        return (values[listIndex] >> bitIndex & 1ul) != 0;
+    }
+
+    public void Set(int index, bool value = true)
+    {
+        int listIndex = index / sizeof(ulong);
+        int bitIndex = index % sizeof(ulong);
+
+        ulong mask = 1ul << bitIndex;
+        values[listIndex] = (values[listIndex] & ~mask) | (mask * value.ToUlong());
+    }
+
+    private BitArray64 OperateBinary(BitArray64 other, Func<ulong, ulong, ulong> operation)
+    {
+        int maxLength = Math.Max(other.Length, Length);
+        ulong[] newValues = new ulong[maxLength];
+        for(int i = 0; i < maxLength; i++)
+        {
+            ulong thisBits = i < values.Length ? values[i] : 0ul;
+            ulong otherBits = i < other.values.Length ? other.values[i] : 0ul;
+
+            newValues[i] = operation.Invoke(thisBits, otherBits);
+        }
+
+        return new BitArray64(Math.Max(Length, other.Length), newValues);
+    }
+
+    private BitArray64 OperateUnary(Func<ulong, ulong> operation)
+    {
+        ulong[] newValues = new ulong[Length];
+        for(int i = 0; i < Length; i++)
+        {
+            newValues[i] = operation.Invoke(values[i]);
+        }
+
+        return new BitArray64(Length, newValues);
+    }
+
+    public BitArray64 And(BitArray64 other) => OperateBinary(other, (a, b) => a & b);
+
+    public BitArray64 Or(BitArray64 other) => OperateBinary(other, (a, b) => a | b);
+
+    public BitArray64 Not() => OperateUnary(a => ~a);
+
+    public static BitArray64 operator &(BitArray64 a, BitArray64 b) => a.And(b);
+    public static BitArray64 operator |(BitArray64 a, BitArray64 b) => a.Or(b);
+    public static BitArray64 operator ~(BitArray64 a) => a.Not();
+
+
+}
--- DataStructures/bitStructures/BitSet.cs
+++ DataStructures/bitStructures/BitSet.cs
@@ -1,100 +1,47 @@
 ﻿
+using System.Collections;
+
 namespace DataStructures.bitStructures;
 
-public class BitSet<T>
+public class BitSet<T> where T : notnull
 {
-    private readonly BitUniverse<T> universe;
+    private readonly IBitUniverse<T> universe;
 
-    private readonly List<ulong> list;
+    private readonly BitArray array;
 
-    public BitSet(BitUniverse<T> universe)
+    public BitSet(IBitUniverse<T> universe)
     {
         this.universe = universe;
-        list = new List<ulong>();
+        array = new(universe.Length);
     }
 
-    private BitSet(BitUniverse<T> universe, List<ulong> list) : this(universe)
+    private BitSet(IBitUniverse<T> universe, BitArray array) : this(universe)
     {
-        this.list = list;
+        if(universe.Length != array.Length) throw new ArgumentException();
+
+        this.array = array;
     }
 
-    public void Add(T value)
-    {
-        int index = universe.Add(value);
+    public void Add(T value) => array[universe.IndexOf(value)] = true;
 
-        int listIndex = index / sizeof(ulong);
-        while(listIndex >= list.Count) list.Add(0ul);
-        
-        int bitIndex = index % sizeof(ulong);
-        list[listIndex] |= 1ul << bitIndex;
-    }
+    public void Remove(T value) => array[universe.IndexOf(value)] = false;
 
-    public void Remove(T value)
-    {
-        int index = universe.Add(value);
+    public bool Contains(T value) => array[universe.IndexOf(value)];
 
-        int listIndex = index / sizeof(ulong);
-        if(listIndex >= list.Count) return;
+    public BitSet<T> Union(BitSet<T> other) => new(universe, array.Or(other.array));
 
-        int bitIndex = index % sizeof(ulong);
-        list[listIndex] &= ~(1ul << bitIndex);
-    }
+    public BitSet<T> Intersect(BitSet<T> other) => new(universe, array.And(other.array));
 
-    public bool Contains(T value)
-    {
-        int index = universe.Add(value);
-
-        int listIndex = index / sizeof(ulong);
-        if(listIndex >= list.Count) return false;
-        
-        int bitIndex = index % sizeof(ulong);
-        return ((list[listIndex] >> bitIndex) & 1ul) != 0;
-    }
-
-    private BitSet<T> Operate(BitSet<T> other, Func<ulong, ulong, ulong> operation)
-    {
-        //alternativly create a new combined universe?
-        if(!universe.Equals(other.universe))
-        {
-            throw new ArgumentException("the universes did not match!");
-        }
-
-        int maxCount = Math.Max(other.list.Count, list.Count);
-        List<ulong> newValues = new();
-        for(int i = 0; i < maxCount; i++)
-        {
-            ulong thisBits = i < list.Count ? list[i] : 0ul;
-            ulong otherBits = i < other.list.Count ? other.list[i] : 0ul;
-
-            newValues.Add(operation.Invoke(thisBits, otherBits));
-        }
-
-        return new BitSet<T>(universe, newValues);
-    }
-
-    public BitSet<T> Union(BitSet<T> other) => Operate(other, (a, b) => a | b);
-
-    public BitSet<T> Intersect(BitSet<T> other) => Operate(other, (a, b) => a & b);
+    public BitSet<T> Compliment() => new(universe, array.Not());
 
 }
 
-public class BitUniverse<T>
+public interface IBitUniverse<T> where T : notnull
 {
+    public int Length { get; }
 
-    private readonly List<T> values;
-
-    public BitUniverse()
-    {
-        values = new();
-    }
-
-    //when something is added then it can't be removed
-    public int Add(T value)
-    {
-        if (!values.Contains(value)) values.Add(value);
-        return IndexOf(value);
-    }
-
-    public int IndexOf(T value) => values.IndexOf(value);
+    public T ValueOf(int index);
+    
+    public int IndexOf(T value);
 
 }
